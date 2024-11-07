271fd9133d7249e0c3563c4412bb4f36aeea86a1
DiogoROliveira <diogo.rosas.oliveira@ipvc.pt>
2024-11-07 22:46:39 +0000

    login routes to correct place

--- reclama-condo/resources/views/welcome.blade.php
+++ reclama-condo/resources/views/welcome.blade.php
@@ -920,11 +920,19 @@
                     @if (Route::has('login'))
                     <nav class="-mx-3 flex flex-1 justify-end">
                         @auth
+                        @if (Auth::user()->role_id == 2)
                         <a
-                            href="{{ url('/dashboard') }}"
+                            href="{{ route('dashboard') }}"
                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                             Dashboard
                         </a>
+                        @elseif (Auth::user()->role_id == 1)
+                        <a
+                            href="{{ route('admin.dashboard') }}"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                            Dashboard
+                        </a>
+                        @endif
                         @else
                         <a
                             href="{{ route('login') }}"
